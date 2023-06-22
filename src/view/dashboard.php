207555495ed48cbd0792b2a6651bc790a6115a3a
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-06-22 22:15:35 +0200

    add chat, navbar logo change, remove links from navbar

--- src/view/dashboard.php
+++ src/view/dashboard.php
@@ -1,5 +1,12 @@
 <?php require_once __DIR__ . '/partials/nav.php' ?>
 <div class="body">
+    <div class="chat">
+        <div class="chat-bubble">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="chat-icon">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
+            </svg>
+        </div>
+    </div>
     <div class="tab-section flex flex-wrap">
         <div class="product-management tab flex justify-center flex-column">
             <h2>Product management</h2>
@@ -119,40 +126,41 @@
                         </svg>
                         <p>Woocommerce options</p>
                     </div>
-                    <div class="link ml-5">
+                    <a href="<?= get_admin_url() . "admin.php?page=settings" ?>" class="link ml-5">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                         </svg>
                         <p>Simpler Dashboard options</p>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
+    </div>
 
-    </div>
-    <div class="blob"> </div>
-    <div>
-        <table id="tblUser">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>State of order</th>
-                    <th>Number of products</th>
-                    <th>Date of order</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Test123</td>
-                    <td>processing</td>
-                    <td>2</td>
-                    <td>1.6.2023</td>
-                    <td>401$</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+</div>
+<div class="blob"> </div>
+<div>
+    <table id="tblUser">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>State of order</th>
+                <th>Number of products</th>
+                <th>Date of order</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Test123</td>
+                <td>processing</td>
+                <td>2</td>
+                <td>1.6.2023</td>
+                <td>401$</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </div>
 
 <script>
--- src/view/partials/nav.php
+++ src/view/partials/nav.php
@@ -1,8 +1,5 @@
 <div class="nav">
     <div class="left-panel">
-        <div>
-            <img src="<?= WP_PLUGIN_URL . '/simpler-woocommerce/storage/logo2.png' ?>" alt="logo goes here" class="logo" />
-        </div>
         <div class="nav-child flex align-center">
             <div>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="nav-icon">
@@ -15,27 +12,20 @@
         </div>
         <div class="nav-child flex align-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="nav-icon">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <p>
-                10 unfinished orders
-            </p>
+            <div>
+                <?= date('H:i') ?>
+            </div>
         </div>
     </div>
-    <div class="right-panel">
-        <div class="nav-child">
-            <?php
-            if ($_SERVER['QUERY_STRING'] === 'page=settings') {
-                echo "<a href=" . get_admin_url() . "admin.php?page=simpler_woo>Dashboard</a>";
-            } elseif ($_SERVER['QUERY_STRING'] === 'page=simpler_woo') {
-                echo "<a href=" . get_admin_url() . "admin.php?page=settings>Settings</a>";
-            }
-            ?>
+
+    <div class="flex align-center">
+        <div>
+            <img src="<?= WP_PLUGIN_URL . '/simpler-woocommerce/storage/logo.png' ?>" alt="logo goes here" class="logo" />
         </div>
-        <div class="nav-child">
-            <p>
-                chat goes here
-            </p>
+        <div>
+            <h3 class="text-bold text-xl">Simpler</h3>
         </div>
     </div>
 </div>
